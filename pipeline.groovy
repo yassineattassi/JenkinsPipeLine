6392c28e5e2acb78d6ab8313fcd4a0b66b8aa508
yassineattassi <40597246+yassineattassi@users.noreply.github.com>
2018-06-27 12:13:01 +0100

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,17 @@
 
 pipeline {
     agent any 
+    tools { 
+        maven 'Maven 3.3.9' 
+    }
     stages {
         stage('Build') { 
             steps {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '9cae0606-0c73-45c5-ae72-6640a8d6fb31', url: 'https://github.com/gabrielf/maven-samples.git']]])
+                sh 'pwd'
+                sh 'ls'
+                sh 'mvn -Dmaven.test.failure.ignore=true install'
+                
             }
         }
         stage('Test') { 
